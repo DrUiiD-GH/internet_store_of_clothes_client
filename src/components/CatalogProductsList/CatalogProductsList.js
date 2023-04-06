e7d3f232cb1ca16218162d09f823afed0129ec9f
Andrey Brusov <brusov2001@bk.ru>
2023-04-06 20:45:24 +0300

    create card product

--- src/components/CatalogProductsList/CatalogProductsList.js
+++ src/components/CatalogProductsList/CatalogProductsList.js
@@ -3,14 +3,15 @@ import {observer} from "mobx-react-lite";
 import styles from './CatalogProductsList.module.css'
 import {Context} from "../../index";
 import ProductItem from "../ProductItem/ProductItem";
+import {Row} from "react-bootstrap";
 
 const CatalogProductsList = observer(() => {
     const {product} = useContext(Context)
 
     return (
-        <div className={styles.itemsList}>
+        <Row className={styles.productsList}>
             {product.products.map( product => <ProductItem key={product.id} product={product}/>)}
-        </div>
+        </Row>
     );
 });
 
--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Col} from "react-bootstrap";
+import styles from './ProductItem.module.css'
 
-const ProductItem = () => {
+const ProductItem = ({product}) => {
+
+
+
+
     return (
-        <Col md={3}>
-            Item
+        <Col md={4}>
+            <div className={styles.card}>
+                <img className={styles.card__img} alt={product.name}/>
+                <div className={styles.card__description}>
+                    <h3 className={styles.description__name}>{product.name}</h3>
+                    <h4 className={styles.description__cat}>Рубашки</h4>
+                    <span className={styles.description__pr}>{product.price} ₽</span>
+                </div>
+            </div>
         </Col>
     );
 };
