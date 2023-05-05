e20bb369aacdf66f222c745bbbbbd7abf4edd5b5
Andrey Brusov <brusov2001@bk.ru>
2023-05-06 00:22:55 +0300

    ready profile page

---
+++ src/components/ProfilePageComponents/UserOrders/OrderPosition/OrderPosition.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './OrderPosition.module.css'
+
+const OrderPosition = ({position, ...props}) => {
+    return (
+        <div className={styles.position__wrap}>
+            <h4 className={styles.position__number}>
+                Заказ №{position.id}
+            </h4>
+            <div className={styles.position__row}>
+                <span className={styles.desc}>Всего товаров: {position.count}</span>
+                <span className={styles.desc}>На общую сумму: {position.total_cost} ₽</span>
+                <button className={styles.desc_button}>Товары в заказе</button>
+            </div>
+            <div className={styles.position__row+' '+styles.position__row_status}>
+                <span className={styles.status_title}>Статус заказа: </span>
+                <span className={styles.status_value}>{position.status}</span>
+            </div>
+        </div>
+    );
+};
+
+export default OrderPosition;
--- src/components/ProfilePageComponents/UserOrders/UserOrders.js
+++ src/components/ProfilePageComponents/UserOrders/UserOrders.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import styles from './UserOrders.module.css'
+import OrderPosition from "./OrderPosition/OrderPosition";
 
 const UserOrders = () => {
+    const orders = [
+        {
+            id:45,
+            count:4,
+            total_cost:'13000',
+            status:'Ожидает отправки'
+        },
+        {
+            id:547,
+            count:2,
+            total_cost:'4800',
+            status:'Создан'
+        }
+    ]
+
     return (
-        <div>
-            Orders
+        <div className={styles.orders__wrap}>
+            <h2 className={styles.orders__title}>Мои заказы</h2>
+            {
+                orders.map(position=>
+                    <OrderPosition position = {position} key={position.id}/>
+                )
+            }
         </div>
     );
 };
